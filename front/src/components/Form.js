0ef4524a0112c005403aef1fa93979d4c559d094
Rares-Hampi <hampimarian1323@gmail.com>
2023-08-12 17:37:12 +0300

    am facut formularele

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -1,8 +1,9 @@
 import React from "react";
-import Modal from "react-bootstrap/Modal";
-import Button from "react-bootstrap/Button";
+import { Modal, Button, Container, ModalBody } from "react-bootstrap";
 
-export default function Form(props) {
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+export default function Formular(props) {
   if (props.form === "logIn") {
     return (
       <Modal
@@ -12,15 +13,61 @@ export default function Form(props) {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Log in</Modal.Title>
         </Modal.Header>
-        <Modal.Body>LogIn</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={props.action}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
+
+        <ModalBody className="d-flex justify-content-center align-items-center">
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+              if (!values.password) {
+                errors.password = "Required";
+              }
+
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form className="d-flex flex-column ">
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" className="mb-3" />
+                <ErrorMessage
+                  name="email"
+                  component="span"
+                  className="mb-3 bg-danger align-self-center p-2 rounded"
+                />
+                <label htmlFor="password">Password</label>
+                <Field type="password" name="password" className="mb-3" />
+                <ErrorMessage
+                  name="password"
+                  component="span"
+                  className="mb-3 bg-danger align-self-center p-2 rounded"
+                />
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="w-50 align-self-center"
+                >
+                  Log in
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </ModalBody>
       </Modal>
     );
   } else if (props.form === "singUp") {
@@ -32,15 +79,70 @@ export default function Form(props) {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Sing up</Modal.Title>
         </Modal.Header>
-        <Modal.Body>SingUp</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={props.action}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
+
+        <ModalBody className="d-flex justify-content-center align-items-center">
+          <Formik
+            initialValues={{ email: "", password: "", name: "" }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+              if (!values.password) {
+                errors.password = "Required";
+              }
+              if (!values.name) {
+                errors.name = "Required";
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form className="d-flex flex-column ">
+                <label htmlFor="name">Name</label>
+                <Field type="name" name="name" className="mb-3" />
+                <ErrorMessage
+                  name="name"
+                  component="span"
+                  className="mb-3 bg-danger align-self-center p-2 rounded"
+                />
+                <label htmlFor="email">Email</label>
+                <Field type="email" name="email" className="mb-3" />
+                <ErrorMessage
+                  name="email"
+                  component="span"
+                  className="mb-3 bg-danger align-self-center p-2 rounded"
+                />
+                <label htmlFor="password">Password</label>
+                <Field type="password" name="password" className="mb-3" />
+                <ErrorMessage
+                  name="password"
+                  component="span"
+                  className="mb-3 bg-danger align-self-center p-2 rounded"
+                />
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="w-50 align-self-center"
+                >
+                  Sing up
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </ModalBody>
       </Modal>
     );
   }
