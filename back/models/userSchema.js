b4b1b882b4310619cfc945399d36b44118fe30cd
Rares-Hampi <hampimarian1323@gmail.com>
2023-08-19 14:13:46 +0300

    last changes

--- back/models/userSchema.js
+++ back/models/userSchema.js
@@ -6,8 +6,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   mail: { type: String, required: true },
   password: { type: String, required: true },
-  age: { type: Number, required: false },
-  about: { type: String, required: false },
+  age: { type: Number, default: "" },
+  about: { type: String, default: "" },
 });
 
 const User = mongoose.model("User", userSchema);
